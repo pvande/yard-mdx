778ad717fe1f522b82bb6ae37c2fc0ed9a5a6609
Pieter van de Bruggen <pvande@gmail.com>
2024-10-21 15:55:50 -0700

    Fixups

--- lib/templates/default/fulldoc/mdx/setup.rb
+++ lib/templates/default/fulldoc/mdx/setup.rb
@@ -5,10 +5,9 @@ def init
   options.objects = run_verifier(options.objects)
 
   generate_docs_config
-  generate_homepage
 
   options.objects.each { |x| serialize(x) }
-  # options.files.each { |x| serialize(x) }
+  options.files.each { |x| serialize(x) }
 end
 
 def generate_docs_config
@@ -22,55 +21,77 @@ def generate_docs_config
   end
 
   config["sidebar"] ||= []
+
+  group = config["sidebar"].shift
+  if group.nil?
+    config["sidebar"].unshift(group = {})
+  elsif group["group"]
+    config["sidebar"].unshift(group)
+    config["sidebar"].unshift(group = {})
+  end
+  sidebar_docs(group["pages"] ||= [])
+
   config["sidebar"] << {
     group: "API Docs",
-    pages: [
-      *sidebar_for(Registry.root)
-    ]
+    pages: sidebar_api(Registry.root)
   }
 
   serializer.serialize("config.json", JSON.pretty_generate(config))
-end
-
-def generate_homepage
-  return unless options.readme
-  serializer.serialize("index.mdx", T("readme").run(options))
 end
 
 def generate_assets
   Array(assets).each { |asset| serialize(asset) }
 end
 
-def sidebar_for(object)
+def sidebar_docs(pages)
+  if options.readme
+    pages << { title: "Home", href: "/index" }
+  end
+
+  return { pages: [] }
+end
+
+def sidebar_api(object)
   config = {}
   namespaces = object.children.grep(YARD::CodeObjects::NamespaceObject)
   config[:icon] = icon_for(object)
   if namespaces.any?
     config[:group] = object.type == :root ? object.title : object.name
     config[:href] = serializer.serialized_path(object).delete_suffix(".mdx") if has_documentation?(object)
-    config[:pages] = namespaces.flat_map { |child| sidebar_for(child) }
+    config[:pages] = namespaces.flat_map { |child| sidebar_api(child) }
   else
     config[:title] = object.type == :root ? object.title : object.name
     config[:href] = serializer.serialized_path(object).delete_suffix(".mdx")
   end
-  [config]
+
+  return [config]
 end
 
 def serialize(obj)
+  if obj == options.readme
+    options.serializer.serialize("index.mdx", format_file(obj.contents))
+    return
+  end
+
   contents = case obj
   when String
     file(obj)
   when YARD::CodeObjects::ExtraFileObject
-    obj.contents
+    format_file(obj.contents)
   else
-    obj.format(options.to_hash.slice(:format, :template))
+    erb("page_header") + obj.format(options.to_hash.slice(:format, :template))
   end
 
   options.serializer.serialize(obj, contents)
 end
 
+def format_file(contents)
+  erb("page_header") +
+  resolve_alerts(resolve_links(contents))
+end
+
 def has_documentation?(obj)
   return true unless obj.docstring.blank?(true)
-  return true unless obj.children.empty?
+  # return true unless obj.children.empty?
   return false
 end
--- lib/templates/default/setup.rb
+++ lib/templates/default/setup.rb
@@ -18,6 +18,8 @@ def param_component(tag)
 
   if propstr.empty?
     "#{resolve_links(tag.text).strip}\n"
+  elsif props.key?(:type) && !props.key?(:name) && tag.text.strip.empty?
+    "<Type name=#{props[:type].inspect} />\n"
   elsif props.key?(:type) && !props.key?(:name)
     "<Type name=#{props[:type].inspect} /> &mdash;\n#{resolve_links(tag.text.strip)}\n"
   elsif tag.text && !tag.text.empty?
@@ -57,6 +59,102 @@ def indent(text, len = 2)
   text.strip!
   return "" if text.empty?
   text.gsub(/^/, " " * len)
+end
+
+def format_types(list, brackets = true)
+  list.nil? || list.empty? ? "" : (brackets ? "(#{list.join(", ")})" : list.join(", "))
+end
+
+def format_block(object)
+  if object.has_tag?(:yield) && object.tag(:yield).types
+    params = object.tag(:yield).types
+  elsif object.has_tag?(:yieldparam)
+    params = object.tags(:yieldparam).map(&:name)
+  elsif object.has_tag?(:yield)
+    return "{ ... }"
+  else
+    params = nil
+  end
+
+  params && h("{|" + params.join(", ") + "| ... }")
+end
+
+def signature(meth, full_attr_name = true)
+  meth = convert_method_to_overload(meth)
+
+  type = signature_types(meth)
+  type = "# => #{type}" if type && !type.empty?
+  name = full_attr_name ? meth.name : meth.name.to_s.gsub(/^(\w+)=$/, '\1')
+  blk = format_block(meth)
+  args = !full_attr_name && meth.writer? ? "" : format_args(meth)
+  ["#{h(name)}#{args}", blk, type].compact.join(" ")
+end
+
+def signature_types(meth, link = true)
+  meth = convert_method_to_overload(meth)
+  if meth.respond_to?(:object) && !meth.has_tag?(:return)
+    meth = meth.object
+  end
+
+  type = options.default_return || ""
+  if meth.tag(:return) && meth.tag(:return).types
+    types = meth.tags(:return).map {|t| t.types ? t.types : [] }.flatten.uniq
+    first = format_types([types.first], false)
+    if types.size == 2 && types.last == 'nil'
+      type = first + '?'
+    elsif types.size == 2 && types.last =~ /^(Array)?<#{Regexp.quote types.first}>$/
+      type = first + '+'
+    elsif types.size > 2
+      type = [first, '...'].join(', ')
+    elsif types == ['void'] && options.hide_void_return
+      type = ""
+    else
+      type = format_types(types, false)
+    end
+  elsif !type.empty?
+    type = format_types([type], false)
+  end
+  type = "#{type} " unless type.empty?
+  type
+end
+
+def resolve_alerts(source)
+  source.gsub(/^> \[!(NOTE|TIP|IMPORTANT|WARNING|CAUTION)\]\n((?:^> .*\n)+)/) do |*args|
+    type = $1
+    content = indent($2.gsub(/^> /, ""))
+    case type
+    when "NOTE"
+      <<~MDX
+        <Callout className="border bg-black/5 dark:bg-white/5">
+        #{content}
+        </Callout>
+      MDX
+    when "TIP"
+      <<~MDX
+        <Success>
+        #{content}
+        </Success>
+      MDX
+    when "IMPORTANT"
+      <<~MDX
+        <Info>
+        #{content}
+        </Info>
+      MDX
+    when "WARNING"
+      <<~MDX
+        <Warning>
+        #{content}
+        </Warning>
+      MDX
+    when "CAUTION"
+      <<~MDX
+        <Error>
+        #{content}
+        </Error>
+      MDX
+    end
+  end
 end
 
 def html_syntax_highlight(str)
